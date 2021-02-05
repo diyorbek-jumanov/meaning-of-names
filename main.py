47cad8bd68f8f368c2d97176e588be697e31491f
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-05 03:52:02 +0000

    getUpdates

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import os
 token = os.environ['token']
 
 
+def getUpdates():
+    url_up = f'https://api.telegram.org/bot{token}/getUpdates'
+    respons_u = requests.get(url=url_up)
+    data = respons_u.json()['result']
+    last_msg = data[-1]
+
+    return last_msg
+
+
+
+    
+
 # url = 'https://ismlar.com/uz/name/%D0%94%D0%B8%D1%91%D1%80'
 
 # r = requests.get(url)
