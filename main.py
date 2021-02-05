bab4f91939a40e8ba4a174a4efe946a188c41eec
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-05 15:01:14 +0000

    name , mean, text

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import os
 
-token = os.environ['token']
+# token = os.environ['token']
+token = '1529375049:AAGsVK_oy8Bs_mkcZitgGSs63GBH46ErUas'
 
 
 def getUpdates():
@@ -13,16 +14,31 @@ def getUpdates():
     return last_msg
 
 
+def Mean_name(text):
+    url = f'https://ismlar.com/uz/search/{text}'
+    r = requests.get(url)
 
-    
+    # idx1 = r.text.index('meta property="og:description" content="')
+    ism = r.text[r.text.find(text):r.text.find(' ', r.text.find(text))]
+    lang = r.text[r.text.find('<i class="fas fa-tag"></i>', r.text.find(text))+59:r.text.find('\n', r.text.find('<i class="fas fa-tag"></i>')+30)]
+    mano = r.text[r.text.find('<p class="text-size-5">', r.text.find(text))+23:r.text.find('</p>', r.text.find('<p class="text-size-5">')+28)]
+    print(r.text)
+    print(ism)
+    print(lang)
+    print(mano)
+    # print(r.url)
+    return mano
 
-# url = 'https://ismlar.com/uz/name/%D0%94%D0%B8%D1%91%D1%80'
 
-# r = requests.get(url)
+msg_id = getUpdates()['message']['message_id']
+while True:
+    data = getUpdates()
+    last_msg_id = data['message']['message_id']
+    chat_id = data['message']['from']['id']
+    msg_text = data['message']['text']
 
-# # print(r.text)
+    if msg_id != last_msg_id:
+        mean = Mean_name(msg_text)
+        msg_id = last_msg_id
 
-# idx1 = r.text.index('meta property="og:description" content="')
-# mano = r.text[idx1+40:r.text.index('"', idx1+41)]
 
-# print(mano)
