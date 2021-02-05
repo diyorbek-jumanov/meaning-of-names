dea0aadefdc36a51feaeff52259e7a31d26af72d
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-05 16:12:24 +0000

    checking

--- main.py
+++ main.py
@@ -10,35 +10,39 @@ def getUpdates():
     respons_u = requests.get(url=url_up)
     data = respons_u.json()['result']
     last_msg = data[-1]
-
+    # print(last_msg)
     return last_msg
 
 
 def Mean_name(text):
-    url = f'https://ismlar.com/uz/search/{text}'
+    
+    url = f'https://ismlar.com/uz/search/{text.title()}'
     r = requests.get(url)
+    
+    data = r.text
 
-    # idx1 = r.text.index('meta property="og:description" content="')
-    ism = r.text[r.text.find(text):r.text.find(' ', r.text.find(text))]
-    lang = r.text[r.text.find('<i class="fas fa-tag"></i>', r.text.find(text))+59:r.text.find('\n', r.text.find('<i class="fas fa-tag"></i>')+30)]
-    mano = r.text[r.text.find('<p class="text-size-5">', r.text.find(text))+23:r.text.find('</p>', r.text.find('<p class="text-size-5">')+28)]
-    print(r.text)
-    print(ism)
+    i_1 = data.find('<i class="fas fa-tag"></i>') + 28
+    i_2 = data.find("\n", i_1)
+
+    lang = data[i_1:i_2]
     print(lang)
+    i_3 = data.find('<p class="text-size-5">') + 24
+    i_4 = data.find('</p>', i_3)
+    mano = data[i_3:i_4]
     print(mano)
-    # print(r.url)
-    return mano
+    # idx1 = data.find()
+Mean_name("Faxriddin")
+# Mean_name(msg_text)
 
+# msg_id = 0
+# while True:
+#     data = getUpdates()
+#     last_msg_id = data['message']['message_id']
+#     chat_id = data['message']['from']['id']
+#     msg_text = data['message']['text']
 
-msg_id = getUpdates()['message']['message_id']
-while True:
-    data = getUpdates()
-    last_msg_id = data['message']['message_id']
-    chat_id = data['message']['from']['id']
-    msg_text = data['message']['text']
-
-    if msg_id != last_msg_id:
-        mean = Mean_name(msg_text)
-        msg_id = last_msg_id
+#     if msg_id != last_msg_id:
+        
+#         msg_id = last_msg_id
 
 
