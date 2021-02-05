a86f28b1e5a9b1ada17fb57df4af5753626715df
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-05 03:45:13 +0000

    found mean

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ url = 'https://ismlar.com/uz/name/%D0%94%D0%B8%D1%91%D1%80'
 
 r = requests.get(url)
 
-print(r.text)
+# print(r.text)
+
+idx1 = r.text.index('meta property="og:description" content="')
+mano = r.text[idx1+40:r.text.index('"', idx1+41)]
+
+print(mano)
